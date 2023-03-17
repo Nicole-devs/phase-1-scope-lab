beb57e867415211654b7917d196fe08a1d9c102c
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-03-17 20:51:05 +0300

    Scope p1

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+ return customerName = 'bob'.toUpperCase();
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'not bob';
+}
+overwriteBestCustomer();
+console.log(bestCustomer);
+
+
+var bestCustomer = 'undefined';
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+overwriteBestCustomer();
+console.log(bestCustomer);
+
+const leastFavoriteCustomer = "Mark"
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "John";
+}
+
+function test_changeLeastFavoriteCustomer() {
+  changeLeastFavoriteCustomer();
+  assert.equal(leastFavoriteCustomer, "John");
+}
